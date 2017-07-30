076100745cf19bb97140a24f4759018ebc17bc01
dcodeIO <dcode@dcode.io>
2017-07-30 20:21:33 +0200

    Attempt for better error reporting
    This should trigger a build

--- tests/index.js
+++ tests/index.js
@@ -7,29 +7,39 @@ var binaryen = require("..");
 
 var mod, txt, bin;
 
-assert.doesNotThrow(function() {
+try {
   mod = new binaryen.Module();
   mod.addFunction("main", mod.addFunctionType("i", binaryen.i32, []), [], mod.return(mod.i32.const(0)));
   mod.addExport("main", "main");
-}, "constructing a simple module should not throw");
+} catch (e) {
+  assert.fail(e, undefined, "constructing a simple module should not throw (" + e + ")");
+}
 
-assert.doesNotThrow(function() {
+try {
   txt = mod.emitText();
   assert(typeof txt === "string" && txt.length, "text output should be a non-empty string");
-}, "emitting text should not throw");
+ } catch (e) {
+   assert.fail(e, undefined, "emitting text should not throw (" + e + ")");
+ }
 
-assert.doesNotThrow(function() {
+try {
   bin = mod.emitBinary();
   assert(bin && bin.length, "binary output should be a non-empty array-like object");
-}, "emitting binary should not throw");
+} catch (e) {
+  assert.fail(e, undefined, "emitting binary should not throw (" + e + ")");
+}
 
-assert.doesNotThrow(function() {
+try {
   assert(mod.validate(), "the module should be valid");
-}, "validating the module should not throw");
+} catch (e) {
+  assert.fail(e, undefined, "validating the module should not throw (" + e + ")");
+}
 
-assert.doesNotThrow(function() {
+try {
   mod.optimize();
-}, "optimizing the module should not throw");
+} catch (e) {
+  assert.fail(e, undefined, "optimizing the module should not throw (" + e + ")");
+}
 
 fs.readFile(__dirname + "/fixtures/index.text", function(err, contents) {
   assert.ifError(err, "the text fixture should exist");
@@ -40,3 +50,5 @@ fs.readFile(__dirname + "/fixtures/index.wasm", function(err, contents) {
   assert.ifError(err, "the binary fixture should exist");
   assert(Buffer.compare(bin, contents) === 0, "binary output should match the binary fixture");
 });
+
+console.log("ok");
