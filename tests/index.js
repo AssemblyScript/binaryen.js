e3c3ac8601a75daf3d9abe9de0ba5e0b6fb0ba9d
dcodeIO <dcode@dcode.io>
2017-08-17 02:03:48 +0200

    Use tape for testing

--- tests/index.js
+++ tests/index.js
@@ -1,54 +1,104 @@
 var fs = require("fs");
-var assert = require("assert");
-var binaryen = require("..");
+var test = require("tape");
 
 // This test case exists to catch the most obvious issues before pushing the binary back to GitHub.
 // It's not intended to be a full test suite, but feel free to extend it / send a PR if necessary!
 
-var mod, txt, bin;
-
-try {
-  mod = new binaryen.Module();
-  mod.addFunction("main", mod.addFunctionType("i", binaryen.i32, []), [], mod.return(mod.i32.const(0)));
-  mod.addExport("main", "main");
-} catch (e) {
-  assert.fail(e, undefined, "constructing a simple module should not throw (" + e + ")");
-}
-
-try {
-  txt = mod.emitText();
-  assert(typeof txt === "string" && txt.length, "text output should be a non-empty string");
- } catch (e) {
-   assert.fail(e, undefined, "emitting text should not throw (" + e + ")");
- }
-
-try {
-  bin = mod.emitBinary();
-  assert(bin && bin.length, "binary output should be a non-empty array-like object");
-} catch (e) {
-  assert.fail(e, undefined, "emitting binary should not throw (" + e + ")");
-}
-
-try {
-  assert(mod.validate(), "the module should be valid");
-} catch (e) {
-  assert.fail(e, undefined, "validating the module should not throw (" + e + ")");
-}
-
-try {
-  mod.optimize();
-} catch (e) {
-  assert.fail(e, undefined, "optimizing the module should not throw (" + e + ")");
-}
-
-fs.readFile(__dirname + "/fixtures/index.text", function(err, contents) {
-  assert.ifError(err, "the text fixture should exist");
-  assert.strictEqual(txt, contents.toString().replace(/\r?\n/g, "\n"), "text output should match the text fixture");
+var binaryen;
+test("requiring binaryen", function(test) {
+  test.doesNotThrow(function() {
+    binaryen = require("..");
+  });
+  test.end();
 });
 
-fs.readFile(__dirname + "/fixtures/index.wasm", function(err, contents) {
-  assert.ifError(err, "the binary fixture should exist");
-  assert(Buffer.compare(bin, contents) === 0, "binary output should match the binary fixture");
+var mod;
+test("constructing a module", function(test) {
+  test.doesNotThrow(function() {
+    mod = new binaryen.Module();
+  });
+  test.end();
 });
 
-console.log("ok");
+var ftype;
+test("adding a function type", function(test) {
+  test.doesNotThrow(function() {
+    ftype = mod.addFunctionType("i", binaryen.i32, []);
+  });
+  test.end();
+});
+
+var expr;
+test("creating an expression", function(test) {
+  test.doesNotThrow(function() {
+    expr = mod.i32.const(0);
+  });
+  test.end();
+});
+
+var stmt;
+test("creating a statement", function(test) {
+  test.doesNotThrow(function() {
+    stmt = mod.return(expr);
+  });
+  test.end();
+});
+
+test("adding a function", function(test) {
+  test.doesNotThrow(function() {
+    mod.addFunction("main", ftype, [], stmt);
+  });
+  test.end();
+});
+
+test("adding an export", function(test) {
+  test.doesNotThrow(function() {
+    mod.addExport("main", "main");
+  });
+  test.end();
+});
+
+var text;
+test("emitting text", function(test) {
+  test.doesNotThrow(function() {
+    text = mod.emitText();
+  });
+  test.ok(typeof text === "string" && text.length, "should return be a non-empty string");
+  test.end();
+});
+
+var binary;
+test("emitting binary", function(test) {
+  test.doesNotThrow(function() {
+    binary = mod.emitBinary();
+  });
+  test.ok(binary && binary.length, "should return a non-empty array-like object");
+  test.end();
+});
+
+test("validating the module", function(test) {
+  var valid;
+  test.doesNotThrow(function() {
+    valid = mod.validate();
+  });
+  test.ok(valid, "the module should be valid");
+  test.end();
+});
+
+test("optimizing the module", function(test) {
+  test.doesNotThrow(function() {
+    mod.optimize();
+  });
+  test.end();
+});
+
+test("fixtures", function(test) {
+
+  var textComp = fs.readFileSync(__dirname + "/fixtures/index.text").toString().replace(/\r?\n/g, "\n");
+  test.strictEqual(text, textComp.toString().replace(/\r?\n/g, "\n"), "should match text output");
+
+  var binaryComp = fs.readFileSync(__dirname + "/fixtures/index.wasm");
+  test.ok(Buffer.compare(binary, binaryComp) === 0, "should match binary output");
+
+  test.end();
+});
