2034341ace71289acadaf92cbc5256835eb587b2
dcodeIO <dcode@dcode.io>
2017-11-09 16:26:23 +0100

    Windows compat, shared bitcode [ci skip]

--- scripts/build.js
+++ scripts/build.js
@@ -3,8 +3,9 @@ var fs   = require("fs");
 var path = require("path");
 var child_process = require("child_process");
 
-var sourceDirectory = "binaryen/src";
-var emscriptenDirectory = process.env.EMSCRIPTEN || "./emscripten";
+var projectDirectory = ".";
+var sourceDirectory = path.join("binaryen", "src");
+var emscriptenDirectory = process.env.EMSCRIPTEN || path.join(".", "emscripten");
 
 // collect source files
 var sourceFiles = [];
@@ -20,43 +21,82 @@ var sourceFiles = [];
   "wasm-emscripten.cpp"
 ]
 .forEach(pattern => {
-  var matches = glob.sync(sourceDirectory + "/" + pattern);
+  var matches = glob.sync(pattern = sourceDirectory + "/" + pattern);
   if (!matches.length)
-    throw Error("no matches for pattern '" + sourceDirectory + "/" + pattern + "' - did the directory structure change?");
-  Array.prototype.push.apply(sourceFiles, matches);
+    throw Error("no matches for pattern '" + pattern + "' - did the directory structure change?");
+  Array.prototype.push.apply(sourceFiles, matches.map(path.normalize));
 });
-console.log("Collected " + sourceFiles.length + " source files:\n  " + sourceFiles.join("\n  "));
+console.log("Collected " + sourceFiles.length + " source files:\n\n  " + sourceFiles.join("\n  ") + "\n");
 
 // determine exported functions
-var headerSource = require("fs").readFileSync(sourceDirectory + "/binaryen-c.h", "utf8");
+var headerSource = require("fs").readFileSync(path.join(sourceDirectory, "binaryen-c.h"), "utf8");
 var exportedFunctions = [];
 var expression = /\b(Binaryen\w+)\([^)]*\);/g;
 var match;
 while (match = expression.exec(headerSource))
   if (exportedFunctions.indexOf(match[1]) < 0)
     exportedFunctions.push(match[1]);
-console.log("\nFound " + exportedFunctions.length + " exported functions:\n  " + exportedFunctions.join("\n  "));
+console.log("Found " + exportedFunctions.length + " exported functions:\n\n  " + exportedFunctions.join("\n  ") + "\n");
 
-// compile
-var cmd = '"' + path.join(emscriptenDirectory, "em++") + '"';
-var arg = [
+var commonOptions = [
   "-s", "ALLOW_MEMORY_GROWTH=1",
   "-s", "DEMANGLE_SUPPORT=1",
   "-s", "DISABLE_EXCEPTION_CATCHING=0",
   "-s", "ELIMINATE_DUPLICATE_FUNCTIONS=1",
-  "-s", "EXPORTED_FUNCTIONS='[" + exportedFunctions.map(name => "\"_" + name + "\"").join(",") + "]'",
+  "-s", /^win/.test(process.platform)
+    ? "EXPORTED_FUNCTIONS=\"[" + exportedFunctions.map(name => "\\\"_" + name + "\\\"").join(",") + "]\""
+    : "EXPORTED_FUNCTIONS='[" + exportedFunctions.map(name => "\"_" + name + "\"").join(",") + "]'",
   "--memory-init-file", "0",
   "--llvm-lto", "1",
-  "--closure", "1",
-  "--pre-js", sourceDirectory + "/js/binaryen.js-pre.js",
-  "--post-js", sourceDirectory + "/js/binaryen.js-post.js",
   "-std=c++11",
-  "-I" + sourceDirectory + "/",
-  "-Oz",
-  "-o", "index.js"
-].concat(sourceFiles);
-console.log("\nCompiling with: " + cmd + " " + arg.join(" "));
-var ret = child_process.spawnSync(cmd, arg, { stdio: "inherit", shell: true });
-if (ret.status)
-  throw Error("exited with " + ret.status + " (see above for details)");
-console.log("ok");
+  "-I" + sourceDirectory
+];
+
+function precompile() {
+  var cmd = '"' + path.join(emscriptenDirectory, "em++") + '"';
+  var arg = commonOptions.concat([
+    "-o", "shared.bc"
+  ]).concat(sourceFiles);
+  console.log(cmd + " " + arg.join(" ") + "\n");
+  var proc = child_process.spawnSync(cmd, arg, { stdio: "inherit", shell: true });
+  if (proc.error)
+    throw proc.error;
+  if (proc.status !== 0)
+    throw Error("exited with " + proc.status);
+}
+
+function compile(options) {
+  var cmd = '"' + path.join(emscriptenDirectory, "em++") + '"';
+  var arg = commonOptions.concat([
+    "--closure", "1",
+    "--pre-js", options.pre,
+    "--post-js", options.post,
+    "-Oz",
+    "-o", options.out,
+    "-Wno-almost-asm",
+    "shared.bc"
+  ]);
+  console.log(cmd + " " + arg.join(" ") + "\n");
+  var proc = child_process.spawnSync(cmd, arg, { stdio: "inherit", shell: true });
+  if (proc.error)
+    throw proc.error;
+  if (proc.status !== 0)
+    throw Error("exited with " + proc.status);
+}
+
+console.log("Compiling shared bitcode ...\n");
+precompile();
+
+console.log("\nCompiling binaryen.js ...\n");
+compile({
+  pre: path.join(sourceDirectory, "js", "binaryen.js-pre.js"),
+  post: path.join(sourceDirectory, "js", "binaryen.js-post.js"),
+  out: "index.js"
+});
+
+// console.log("\nCompiling minimal.js ...\n");
+// compile({
+//   pre: path.join(projectDirectory, "js", "minimal-pre.js"),
+//   post: path.join(projectDirectory, "js", "minimal-post.js"),
+//   out: "minimal.js"
+// });
