fe8479a136cb1b76a55f88f2d4316b60764423af
dcode <dcode@dcode.io>
2019-11-27 22:40:26 +0100

    Update build script to use cmake

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,4 +1,7 @@
 set -e
 cd binaryen/
-bash build-js.sh
-cp out/binaryen.js ../index.js
+mkdir build
+cd build/
+emconfigure cmake .. -DCMAKE_BUILD_TYPE=Release
+emmake make -j2 binaryen_js
+cp bin/binaryen_js.js ../../index.js
